ce21fb940c1a33974c6b3a38eaa8eb12ec2457dc
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-08-05 20:48:03 -0300

    autenticação com Next auth

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,24 +1,28 @@
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SingInButton (){
-  const isUserLoggedIn = true;
+  const [session] = useSession()
+  console.log(session)
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button "
       className={styles.singInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
-      Jayce Albuquerque
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button
       type="button "
       className={styles.singInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sing in with Github
